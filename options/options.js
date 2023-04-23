1790c8145609a71fbe48a8e08b9320967131b096
Crowdbotics <team@crowdbotics.com>
2023-04-23 01:20:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "long-haze-40130",
+  url: "https://long-haze-40130.botics.co",
+  api: "https://long-haze-40130.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
